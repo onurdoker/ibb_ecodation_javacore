66257cd62ecea2e88283783137d6b9818fece8ed
Onur DÖKER <odoker@msn.com>
2025-02-04 19:26:05 +0300

    java core init

---
+++ src/main/java/com/odoker/Main.java
@@ -0,0 +1,12 @@
+package com.odoker;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i =" + i);
+        }
+    }
+}
